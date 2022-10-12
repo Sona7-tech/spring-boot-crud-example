20148aa0b461b78dccf77665a2357b6d9ce4952f
unknown <sona.ibrahimova.1997@gmail.com>
2022-10-12 11:48:57 -0700

    sprin boot crud API

---
+++ src/main/java/com/java/crud/example/entity/Product.java
@@ -0,0 +1,26 @@
+package com.java.crud.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Generated;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "PRODUCT_TBL")
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private int quantity;
+    private double price;
+}
